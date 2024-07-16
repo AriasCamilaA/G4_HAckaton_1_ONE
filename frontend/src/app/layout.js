a7745488bd9c92ab0920678f4fd6023380e0b037
Camila Arias <ariasruizcamilaa@gmail.com>
2024-07-15 20:46:16 -0500

    fix error

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import { AuthProvider } from '../contexts/AuthContext';
+// import { AuthProvider } from '../contexts/AuthContext';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body className={inter.className}>
-        <AuthProvider>
+        {/* <AuthProvider> */}
           {children}
-        </AuthProvider>
+        {/* </AuthProvider> */}
       </body>
     </html>
   );
